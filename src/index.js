69122cfbf1dd929aef74b5878a2c4dccb9c8b458
Patrick-Tomic <pat12tomic@gmail.com>
2023-11-26 13:24:30 -0500

    base skill bar added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import portait from './Tomic.P.jpg'
+import buildSkills from './skills'
 import './style.scss'
 const header = document.querySelector('.header')
 const img = document.createElement('img')
 img.src = portait
 img.classList.add('portrait')
 header.appendChild(img)
+buildSkills()
---
+++ src/skills.js
@@ -0,0 +1,117 @@
+import java from './java.png'
+import sass from './sass.png'
+import css from './css.png'
+import html from './html.png'
+import js from  './js.png'
+import py from './python.png'
+import sql from './sql.png'
+import wp from './webpack.png'
+import ts from './ts.png'
+export default function buildSkills(){
+    const skills = document.createElement('div')
+    skills.classList.add('wrap')
+    const wrap = document.getElementById('skills')
+    const h2 = document.createElement('h2')
+    h2.innerHTML = "My Skillset"
+    wrap.appendChild(h2)
+    let count = 0
+    while(count < 9){
+        const img = document.createElement('img')
+        const p = document.createElement('p')
+        const card = document.createElement('div')
+        card.classList.add('card')
+        switch(count){
+            case 0:
+                img.title = 'Java'               
+                img.src = java
+                img.alt = 'Java'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+            case 1:
+                img.title = 'Python'
+                img.src = py
+                img.alt = 'Python'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+            case 2:
+                img.title = 'HTML'
+                img.src = html
+                img.alt = 'HTML'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+            case 3:
+                img.title = 'CSS'
+                img.src = css
+                img.alt = 'CSS'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+            case 4:
+                img.title = 'JavaScript'
+                img.src = js
+                img.alt = 'JavaScript'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+            case 5:
+                img.title = 'mySQL'
+                img.src = sql
+                img.alt = 'mySQL'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+            case 6:
+                img.title = 'Sass'
+                img.src = sass
+                img.alt = 'Sass'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+            case 7:
+                img.title = 'Webpack'
+                img.src = wp
+                img.alt = 'Webpack'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+            case 8:
+                img.title = 'TypeScript'
+                img.src = ts
+                img.alt = 'Typescript'
+                p.innerHTML = img.title
+                card.appendChild(img)
+                card.appendChild(p)
+                skills.appendChild(card)
+                count++
+                break
+        }
+        wrap.appendChild(skills)
+    }
+}
