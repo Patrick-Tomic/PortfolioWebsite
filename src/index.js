2d3d1d32721c53989cf761d93992d5b38697bdcd
Patrick-Tomic <pat12tomic@gmail.com>
2023-11-27 13:55:36 -0500

    sidebar functional

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import portait from './Tomic.P.jpg'
 import buildSkills from './skills'
 import './style.scss'
+import buildSideBar from './sidebar'
 const header = document.querySelector('.header')
 const img = document.createElement('img')
 img.src = portait
 img.classList.add('portrait')
 header.appendChild(img)
 buildSkills()
+buildSideBar()
---
+++ src/sidebar.js
@@ -0,0 +1,21 @@
+
+export default function buildSideBar(){
+    const side = document.querySelector('.side')
+    const btn = document.querySelector('.sideBarBtn')
+    const removeBtn = document.createElement('button')
+    removeBtn.classList.add('remove')
+    const sideBar = document.createElement('div')
+    sideBar.setAttribute('class','sideBar')
+    removeBtn.innerHTML = '<svg width="39" height="36" viewBox="0 0 39 36" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="43.6846" height="7.9553" rx="3.97765" transform="matrix(0.74681 0.665037 -0.787811 0.615917 6.26721 1.9906)" fill="white"/> <rect width="42.9248" height="8.08818" rx="4.04409" transform="matrix(-0.678302 0.734783 -0.841244 -0.540656 37.3516 4.37292)" fill="white"/> </svg>'
+    btn.addEventListener('click',()=>{
+    
+       sideBar.setAttribute('id','active')
+    })
+    removeBtn.addEventListener('click',()=>{
+        sideBar.removeAttribute('id','active')
+    })
+   
+    sideBar.appendChild(removeBtn)
+    side.appendChild(sideBar)
+   
+}
